369f55a1cc9d7b8e124f36b8177695722d67f7b7
hectoravilar <168785432+hectoravilar@users.noreply.github.com>
2024-05-23 11:41:41 -0300

    DashboardLTD.py

--- DashboardLTD2.py
+++ DashboardLTD2.py
@@ -1,9 +1,11 @@
 import plotly.graph_objects as go
-
+# Adicionando Bairros e sua media de idade
 x = ['Kobrasol', 'Barreiros', 'Forquilhas', 'Campinas', 'Areias']
 y = [31, 26, 32, 23, 23]
 
-
+# Configurando o hovertext:
+# texto que aparece quando passa o cursor do mouse sobre a barra de informações do gráfico. 
+# Adicionando os nomes,renda,emprego procurado,curso necessário e escolaridade de cada um pedidos
 fig = go.Figure(data=[go.Bar(x=x, y=y,
                              hovertext=['Nome: João e Juliana<br>Média de Renda: R$ 1.800,00<br>Emprego Procurado: Pedreiro(a)<br>Curso Necessário: Construção Civil<br>Escolaridade: Ensino Médio',
                                         'Nome: Maria e Carla<br>Média de Renda: R$ 1.600,00<br>Emprego Procurado: Carpinteiro(a)<br>Curso Necessário: Marcenaria <br>Escolaridade: Ensino Fundamental, Ensino Médio',
@@ -11,16 +13,16 @@ fig = go.Figure(data=[go.Bar(x=x, y=y,
                                         'Nome: Pedro e Ana<br>Média de Renda: R$ 2.000,00<br>Emprego Procurado: Pedreiro(a)<br>Curso Necessário: Construção Civil<br>Escolaridade: Ensino Fundamental, Ensino Médio',
                                         'Nome: Lucas e Camila<br>Média de Renda: R$ 1.600,00<br>Emprego Procurado: Carpinteiro(a)<br>Curso Necessário: Marcenaria<br>Escolaridade: Ensino Fundamental, Ensino Médio', ''])])
 
-
+# Customizando...
 fig.update_traces(marker_color='white', marker_line_color='rgb(8,48,107)', marker_line_width=0.0, opacity=0.7)
 fig.update_layout(
     title_text='Estatísticas de Emprego por Bairro em São José',
-    title_font=dict(size=25, color='white', family="Arial, sans-serif", weight="bold"),  
-    title_x=0.4,  
+    title_font=dict(size=25, color='white', family="Arial, sans-serif", weight="bold"),  # Definindo tamanho,fonte das letras e cor do título
+    title_x=0.4,  # Centralizando o título
     yaxis_title=dict(text='Média de idade', font=dict(size=13, color='white')),
-    font=dict(color='white', size=12, weight="bold"),  # Definindo cor, tamanho e peso da fonte do texto principal
-    plot_bgcolor='rgb(25, 42, 70)',  
-    paper_bgcolor='rgb(25, 42, 70)', 
+    font=dict(color='white', size=12),  # Definindo cor e tamanho da fonte do texto
+    plot_bgcolor='rgb(25, 42, 70)',  # Definindo uma cor de fundo azul escuro 
+    paper_bgcolor='rgb(25, 42, 70)',  # Definindo uma cor de fundo azul escuro
     scene=dict(
         xaxis=dict(
             showbackground=False,
@@ -43,7 +45,7 @@ fig.update_layout(
     )
 )
 
-# Adicionando anotações
+# Adicionando estatística e media de idade de cada bairro
 annotations = [
     dict(
         x='Kobrasol', y=31,
@@ -91,6 +93,6 @@ fig.update_layout(annotations=annotations)
 
 # Adicionando "Bairro" embaixo do gráfico
 fig.add_annotation(x=0.0, y=-0.15, xref='paper', yref='paper', text="Bairros", showarrow=False,
-                   font=dict(size=12, color="white", weight="bold"))
+                   font=dict(size=12, color="white"))
 
 fig.show()
